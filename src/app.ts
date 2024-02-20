1aa58f635865245b81b43ec1b18b6cb30938c02b
Chae Cramb <chaecramb@gmail.com>
2024-02-20 07:13:44 +0000

    feat: create and display plateau

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,28 @@
-export function launchMission() {
+export const launchMission = () => {
   return "Hello, Mars!";
-}
+};
 
-launchMission();
+export const createPlateau = (width: number, height: number) => {
+  let plateau = [];
+  for (let row = 0; row < height; row++) {
+    let line = [];
+    for (let col = 0; col < width; col++) {
+      line.push("..");
+    }
+    plateau.push(line);
+  }
+  return plateau;
+};
+
+export const printPlateau = (plateau: string[][]): void => {
+  for (let row = 0; row < plateau.length; row++) {
+    let line = "";
+    for (let col = 0; col < plateau.length; col++) {
+      line += plateau[row][col];
+    }
+    console.log(line);
+  }
+};
+
+let plateau = createPlateau(10, 10);
+printPlateau(plateau);
