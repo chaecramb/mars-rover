f56c09ec60d64c7a61f53820c7941ac8480e45b9
Chae Cramb <chaecramb@gmail.com>
2024-02-20 07:39:14 +0000

    feat: land rover

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ export const createPlateau = (width: number, height: number) => {
   for (let row = 0; row < height; row++) {
     let line = [];
     for (let col = 0; col < width; col++) {
-      line.push("..");
+      line.push(".");
     }
     plateau.push(line);
   }
@@ -18,11 +18,25 @@ export const printPlateau = (plateau: string[][]): void => {
   for (let row = 0; row < plateau.length; row++) {
     let line = "";
     for (let col = 0; col < plateau.length; col++) {
-      line += plateau[row][col];
+      line += plateau[row][col] + " ";
     }
     console.log(line);
   }
 };
 
 let plateau = createPlateau(10, 10);
+printPlateau(plateau);
+
+let rover = "<";
+
+export const landRover = (
+  plateau: string[][],
+  coordinates: { x: number; y: number },
+  rover: string
+) => {
+  plateau[coordinates.y][coordinates.x] = rover;
+  return plateau;
+};
+
+plateau = landRover(plateau, { x: 0, y: 0 }, rover);
 printPlateau(plateau);
